87ed340cd2176e23b76b026b4c77fe2992e7118f
Frank Orechio <forechio@gmail.com>
2020-10-27 22:30:01 -0500

    Static folder PROPERLY setup ugh.  Baseline test created.

--- planner/tests.py
+++ planner/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+from .models import Person, Availability
 # Create your tests here.
+
+class PersonTestCase(TestCase):
+  def setUp(self):
+    Person.objects.create(name='Test')
+
+  def testPerson(self):
+    self.setUp()
