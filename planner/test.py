a489b06d18e9cd2265ab097186857ad59f5a2ce9
Frank Orechio <forechio@gmail.com>
2020-12-12 20:07:02 -0600

    First major complete version

--- planner/test.py
+++ planner/test.py
@@ -2,9 +2,20 @@ from datetime import datetime, timedelta
 MINIMUM_TIME = 60 * 60 #1 hour min
 MAXIMUM_TIME = 60 * 60 * 2
 CHUNK_TIME = 60 * 30
+
+def timeularize(time_string):
+  h = int(time_string[0:2])
+  m = int(time_string[3:5])
+  s = int(time_string[6:8])
+  now = datetime.now()
+  return now.replace(hour=h, minute=m, second=s, microsecond=0)
+
+#weights
+PARTICIPATION_WEIGHT = 100
+BEST_TIME_RANGE = [timeularize('18:00:00'), timeularize('19:00:00')]#for starts
+BEST_TIME_WEIGHT = 50
 PREFERRED_DAYS = [2,3,4]
-MINIMUM_PARTICIPATION_PERCENT = 0.50 #percent participation
-MINIMUM_PARTICIPATION_COUNT = 4
+PREFERRED_DAYS_WEIGHT = 50
 
 #comes from ORM
 ideal = [
@@ -44,17 +55,19 @@ class Possibility:
     self.duration = end - start
     self.day = day
     self.names = []
+    self.score = 0
   def add_name(self, name):
     if name not in self.names:
       self.names.append(name)
+  def calculate_score(self, party_size):
+    self.score += (len(self.names) / party_size) * PARTICIPATION_WEIGHT
+    if self.start >= BEST_TIME_RANGE[0] and self.start <= BEST_TIME_RANGE[1]:
+      self.score += BEST_TIME_WEIGHT
+    if self.day in PREFERRED_DAYS:
+      self.score += PREFERRED_DAYS_WEIGHT
+    return self.score
   def __str__(self):
     return self.start.strftime('%H:%M:%S') + '-' + self.end.strftime('%H:%M:%S') + ' on ' + self.day + ' with ' + ', '.join(self.names)
-def timeularize(time_string):
-  h = int(time_string[0:2])
-  m = int(time_string[3:5])
-  s = int(time_string[6:8])
-  now = datetime.now()
-  return now.replace(hour=h, minute=m, second=s, microsecond=0)
 def ideal_determinizer(times_list):
   ideal_start = timeularize('00:00:00')
   ideal_end = timeularize('23:59:59')
@@ -114,16 +127,27 @@ def fuzzy_determinizer(times_list):
           continue
 
         possibilities[chunk_start_pretty + '-' + chunk_end_pretty + '-' + str(time1['day'])] = Possibility(chunk_start, chunk_end, str(time1['day']))
+  #put everyone into each possibility they fit with
   for pos in possibilities:
     for time in times_list:
       if int(possibilities[pos].day) != int(time['day']):
         continue
-      print(possibilities[pos].start, timeularize(time['start']), possibilities[pos].start >= timeularize(time['start']))
-      print(possibilities[pos].end, timeularize(time['end']), possibilities[pos].end <= timeularize(time['end']))
       if possibilities[pos].start >= timeularize(time['start']) and possibilities[pos].end <= timeularize(time['end']):
         possibilities[pos].add_name(time['name'])
-  return possibilities
+
+  max = 0
+  biggest_pos = None
+  #calculate possibility scores
+  for pos in possibilities:
+    current_score = possibilities[pos].calculate_score(len(names))
+    if current_score > max:
+      max = current_score
+      biggest_pos = pos
+  return possibilities[biggest_pos]
+
+#assume we don't live in a perfect world
 times_list = less_ideal
+#algorithm go brrr
 times = fuzzy_determinizer(times_list)
-for x in times:
-  print(times[x])
+#show every possibility
+print(times)
