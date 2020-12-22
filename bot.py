b1806a0386f487b6db58f14e03c2ca54c0dd3a95
Frank Orechio <forechio@gmail.com>
2020-12-21 19:47:56 -0600

    Discord bot examples working

--- bot.py
+++ bot.py
@@ -1,11 +1,15 @@
 #https://realpython.com/how-to-make-a-discord-bot-python/
 import os
 import threading
+from db import DB
+
+db = DB()
+db.generate_db()
 
 import discord
 from discord.ext import commands
-from dotenv import load_dotenv
 
+from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
@@ -14,19 +18,12 @@ bot = commands.Bot(command_prefix='!')
 
 @bot.command(name='pp', help='Test code checkination')
 async def test_message(ctx):
-  response = 'does this bad boy respond'
+  response = 'where are all the white women at'
   await ctx.send(response)
 
-from .models import Person
-@bot.command(name='db')
-def db(ctx):
-  t = threading.Thread(target=send, args=[ctx], daemon=True)
-  t.start()
-def send(ctx):
-  ctx.send(Person.objects.all())
+@bot.command(name='peoples')
+async def peoples(ctx):
+  #sends a list of all peoples to server
+  await ctx.send(', '.join([row['name'] for row in db.all('select * from people')]))
 
 bot.run(TOKEN)
-# def launch():
-#   bot.run(TOKEN)
-# t = threading.Thread(target=launch,daemon=True)
-# t.start()
